4135b6c060f3f6cff03f1bdbd4ed4d49ba58f56d
averagua <a.veragua.albornoz@accenture.com>
2020-07-17 21:23:11 -0400

    try to download model from gstorage

--- src/simple_model/predict.py
+++ src/simple_model/predict.py
@@ -1,7 +1,11 @@
 from google.cloud import storage
+import logging
 
-storage_client = storage.Client()
-model_path = 'mockup-model.dat'
-model = model_bucket.get_blob(model_path).download_to_filename('model.dat')
-print(model)
-print(dir(model))
+try:
+    storage_client = storage.Client()
+    model_path = 'mockup-model.dat'
+    model = model_bucket.get_blob(model_path).download_to_filename('model.dat')
+    print(model)
+    print(dir(model))
+except Exception as e:
+    logging.info(e)
